1ab5490478e7dc7b9aa5743347890ad3fe6cf4d1
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-12-02 00:40:11 -0300

    -

--- src/main/java/com/matchmaking/matchmaking_api/config/SwaggerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.matchmaking.matchmaking_api.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-    // SpringDoc já configura tudo automaticamente.
-}
--- src/main/java/com/matchmaking/matchmaking_api/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.matchmaking.matchmaking_api.repository;
-
-import com.matchmaking.matchmaking_api.domain.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, String> {
-}
